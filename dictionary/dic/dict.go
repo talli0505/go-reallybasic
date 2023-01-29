41ac94080a45bb64113ffce330c9ad38c2ac561d
talli0505@naver.com <talli0505@naver.com>
2023-01-29 20:47:11 +0900

    추가

--- dictionary/dic/dict.go
+++ dictionary/dic/dict.go
@@ -8,6 +8,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -17,4 +18,22 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word string, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+
+	// if err == errNotFound {
+	// 	d[word] = def
+	// } else if err == nil {
+	// 	return errWordExists
+	// }
+	return nil
 }
--- dictionary/main.go
+++ dictionary/main.go
@@ -7,11 +7,24 @@ import (
 )
 
 func main() {
-	dictionary := dic.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("second")
+	// dictionary := dic.Dictionary{"first": "First word"}
+	// definition, err := dictionary.Search("second")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definition)
+	// }
+	dictionary := dic.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found", word, "definition: ", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
